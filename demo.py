6bacbabb00c89f69361a85473938db4444517b80
angga <angga.mn@gmail.com>
2017-03-01 07:48:52 +0700

    remove rumus yg belum digunakan

--- demo.py
+++ demo.py
@@ -1,45 +1,46 @@
 import numpy as np
 import time
 
+# formasi NN 3-4-3 untuk MLP
 n_hidden = 4
 n_in = 4
 n_out = 3
 
 learning_rate = 0.01
-momentum = 0.9
 
 np.random.seed(0)
 
 def sigmoid(x):
     return 1.0/(1.0 + np.exp(-x))
 
-def tanh_prime(x):
-    return  1 - np.tanh(x)**2
-
 def train(x, t, V, W, bv, bw):
 
+    
     # forward
+    # hitung input dengan bobot hidden layer matriks 1x4 dot 4x4 menghasilkan matriks 1x4
     A = np.dot(x, V) + bv
-    Z = np.tanh(A)
+    Z = sigmoid(A)
 
+   
+    # hitung hidden layer dengan bobot layer output matriks 1x4 dot 4x3 menghasilkan matriks 1x3
     B = np.dot(Z, W) + bw
     Y = sigmoid(B)
-
-    # backward
+    
+    # backward error ke layer sebelumnya
     Ew = Y - t
-    Ev = tanh_prime(A) * np.dot(W, Ew)
+    Ev = sigmoid(A) * np.dot(W, Ew)
 
+    # error untuk lapisan output di dot outer dengan hasil dari layer hidden
     dW = np.outer(Z, Ew)
+    # error untuk lapisan hidden di dot outer dengan hasil dari layer input
     dV = np.outer(x, Ev)
 
-    # cross-entropy loss
+    # cross-entropy loss tambahan rumus belum digunakan di kelas bu afia
     # loss = -np.mean ( t * np.log(Y) + (1 - t) * np.log(1 - Y) )
 
-    #euclidean square, jarak euclidean antara hasil dan fakta E(y,y')
+    #euclidean square, jarak euclidean antara hasil dan fakta E(y,y') dari wikipedia
     loss =0.5*(np.sum(np.power(Y-t,2)))
 
-    # Note that we use error for each layer as a gradient
-    # for biases
 
     return  loss, (dV, dW, Ev, Ew)
 
@@ -65,15 +66,15 @@ bw = np.zeros(n_out)
 
 params = [V,W,bv,bw]
 
-# Generate some data
+# Generate data
 datatraining = np.loadtxt('iris.data-feature.txt',delimiter=',')
 datalabel = np.loadtxt('iris.data-feature-label.txt',delimiter=',')
 
-print datatraining
-print datalabel
+# print datatraining
+# print datalabel
 
 # Train
-for epoch in range(500):
+for epoch in range(700):
     err = []
     upd = [0]*len(params)
 

f1bec1ffa33e72f73cab50aded144e198ab99949
angga <angga.mn@gmail.com>
2017-03-07 04:42:46 +0700

    add test mechanism

--- demo.py
+++ demo.py
@@ -7,6 +7,7 @@ n_in = 4
 n_out = 3
 
 learning_rate = 0.01
+momentum = 0.9
 
 np.random.seed(0)
 
@@ -26,8 +27,15 @@ def train(x, t, V, W, bv, bw):
     B = np.dot(Z, W) + bw
     Y = sigmoid(B)
     
+
     # backward error ke layer sebelumnya
+    # eror pada lapisan output
     Ew = Y - t
+    # error pada lapisan hidden A adalah matriks 4,1 dikali dengan W=4x3 Ew=3,1
+    # print "x"
+    # print sigmoid(A)
+    # print np.shape([4,5,6])
+    # print "x"
     Ev = sigmoid(A) * np.dot(W, Ew)
 
     # error untuk lapisan output di dot outer dengan hasil dari layer hidden
@@ -57,24 +65,61 @@ def predict(x, V, W, bv, bw):
     else:
         return 'tidak tahu'
 
-# Setup initial parameters
+# Setup initial parameters MLP
+# weight layer hidden
 V = np.random.normal(scale=0.1, size=(n_in, n_hidden))
+# weight layer output
 W = np.random.normal(scale=0.1, size=(n_hidden, n_out))
 
+# Setup initial parameters SLP
+W2 = np.random.normal(scale=0.1, size=(n_in, n_out))
+
+print V
+print W
+
+# bias
 bv = np.zeros(n_hidden)
 bw = np.zeros(n_out)
+# bias SLP
+bw2 = np.zeros(n_out)
 
 params = [V,W,bv,bw]
 
-# Generate data
-datatraining = np.loadtxt('iris.data-feature.txt',delimiter=',')
-datalabel = np.loadtxt('iris.data-feature-label.txt',delimiter=',')
+# split 120 training:30 testing
+datasumber = np.loadtxt('iris.data-feature.txt',delimiter=',')
+datasumberlabel = np.loadtxt('iris.data-feature-label.txt',delimiter=',')
 
-# print datatraining
-# print datalabel
+kelas1=datasumber[0:40,:]
+kelas2=datasumber[50:90,:]
+kelas3=datasumber[100:140,:]
+
+label1=datasumberlabel[0:40,:]
+label2=datasumberlabel[50:90,:]
+label3=datasumberlabel[100:140,:]
+
+# Test data
+kelastest1=datasumber[40:50,:]
+kelastest2=datasumber[90:100,:]
+kelastest3=datasumber[140:150,:]
+
+labeltest1=datasumberlabel[40:50,:]
+labeltest2=datasumberlabel[90:100,:]
+labeltest3=datasumberlabel[140:150,:]
+
+# print kelas1.shape
+
+datatraining=np.concatenate((kelas1,kelas2,kelas3),axis=0)
+
+datalabel = np.concatenate((label1,label2,label3),axis=0)
+
+datatest=np.concatenate((kelastest1,kelastest2,kelastest3),axis=0)
+datalabeltest = np.concatenate((labeltest1,labeltest2,labeltest3),axis=0)
+
+# print datatraining.shape
+# print datalabel.shape
 
 # Train
-for epoch in range(700):
+for epoch in range(10000):
     err = []
     upd = [0]*len(params)
 
@@ -96,9 +141,14 @@ for epoch in range(700):
                 epoch, np.mean( err ), time.clock()-t0 )
 
 # Hasil model
-print "PREDIKSI HASIL CONTOH DATA INDEX 1 dan 139"
-print "prediksi baris ke 1: "
-print predict(datatraining[0], *params)
-print "prediksi baris ke 139: "
-print predict(datatraining[140], *params)
+print "PREDIKSI DATATEST"
+# print datatest
+# print datalabeltest
+print params
+for i in range(datatest.shape[0]):
+    print predict(datatest[i], *params)
+# print "prediksi baris ke 139: "
+# print predict(datatraining[140], *params)
+# print "prediksi baris ke 140: "
+# print predict(datatraining[141], *params)
 
